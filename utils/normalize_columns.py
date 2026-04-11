8cb3354581e8cb1143f19d97020cbf4a20a8b378
Rehan <rehanboyaju523@gmail.com>
2026-04-11 17:26:42 +0545

    Normalized data

---
+++ utils/normalize_columns.py
@@ -0,0 +1,108 @@
+from locale import normalize
+from pathlib import Path
+import pandas as pd
+from datetime import date
+import sys
+
+
+# today = str(date.today())
+# today = "2026-04-09";
+# today = sys.argv[1]
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+final_symbols = set(pd.read_csv("../data/FinalCompanies.csv")["Symbol"].str.strip().str.upper());
+
+def normalize_intraday_columns(input,output) :
+
+    output_dir = Path(output)
+    output_dir.mkdir(parents=True,exist_ok=True)
+
+    
+    for file in Path(input).glob("*.csv") :
+
+
+        df = pd.read_csv(file)
+        symbol = file.stem;
+        # df = df.rename(columns={
+        #     "tradeTime":"transaction_time",
+        #     "contractQuantity":"quantity",
+        #     "contractAmount":"amount"
+        # })
+
+
+        if symbol not in final_symbols:
+            continue
+
+        df.rename(columns={
+            "tradeTime":"transaction_time",
+            "contractQuantity":"quantity",
+            "contractRate":"rate",
+            "contractAmount":"amount"
+        },inplace=True)
+
+        df.to_csv(output_dir / f"{symbol}.csv",index=False)
+
+
+def normalize_interday_columns(input) :
+
+    output_dir = Path(f"data/interday")
+    output_dir.mkdir(parents=True,exist_ok=True)
+    
+    for file in Path(input).glob("*.csv") :
+        df = pd.read_csv(file)
+        symbol = file.stem;
+
+        if symbol not in final_symbols:
+            continue
+
+        df.rename(columns={
+            "published_date":"transaction_time",
+            "traded_quantity":"quantity",
+            "traded_amount":"amount",
+        },inplace=True)
+
+        df.to_csv(output_dir / f"{symbol}.csv",index=False)
+
+
+def normalize_intraday_test_columns(input,output) :
+
+    for date_folder in Path(input).iterdir() :
+
+        if not date_folder.is_dir(): continue
+
+        date = date_folder.name
+
+        output_dir = Path(output) / date
+        output_dir.mkdir(parents=True,exist_ok=True)
+
+
+        for file in date_folder.glob("*.csv") :
+
+            df = pd.read_csv(file)
+            symbol = file.stem
+
+            if symbol not in final_symbols:
+                continue
+
+            df.rename(columns={
+                "Trade Time":"transaction_time",
+                "Quantity":"quantity",
+                "Rate":"rate",
+                "Amount":"amount"
+            },inplace=True)
+
+            df.to_csv(output_dir / f"{symbol}.csv",index=False)
+
+
+normalize_intraday_test_columns(f"{BASE_DIR}/nepse_floorsheet/data/intraday_testing","data/intraday_testing")
+
+
+normalize_intraday_columns(f"{BASE_DIR}/NEPSE_API/data/intraday/2026-04-09","data/intraday/2026-04-09")
+normalize_intraday_columns(f"{BASE_DIR}NEPSE_API/data/intraday/2026-04-10","data/intraday/2026-04-10")
+
+normalize_interday_columns(f"{BASE_DIR}NepseScraper/data/company-wise")
+
+
+
